ba4ded956d8ceac64494c9c9708ceebae93b36ce
oxqlion <rafinaya1127@gmail.com>
2023-10-18 15:08:23 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/cobagithub/MainActivity.kt
+++ app/src/main/java/com/example/cobagithub/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("ehufu7weyhi")
+                    Greeting("saya mau centang")
                 }
             }
         }
