12930b33ab38a454345ad0597c31861b3130eb6c
willyc2004 <willy.cahyadi25@gmail.com>
2023-10-18 15:55:17 +0700

    centang 2

--- app/src/main/java/com/example/cobagithub/MainActivity.kt
+++ app/src/main/java/com/example/cobagithub/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("saya mau centang")
+                    Greeting("saya mau centang 2")
                 }
             }
         }
